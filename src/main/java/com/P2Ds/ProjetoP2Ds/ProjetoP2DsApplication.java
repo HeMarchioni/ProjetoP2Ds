f7f37fd2a8cfdceeadee4e172d136bcf0d54a20a
HeMarchioni <henrique.marchioni@fatec.sp.gov.br>
2021-05-09 23:56:51 -0300

    inicio do projeto

---
+++ src/main/java/com/P2Ds/ProjetoP2Ds/ProjetoP2DsApplication.java
@@ -0,0 +1,13 @@
+package com.P2Ds.ProjetoP2Ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoP2DsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoP2DsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/P2Ds/ProjetoP2Ds/ProjetoP2DsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.P2Ds.ProjetoP2Ds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoP2DsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
